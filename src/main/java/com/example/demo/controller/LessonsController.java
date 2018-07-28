22f5f56c89efeabe362c8f3ac3e8b3f8dd71b17d
Sidharth Jhawar <Sidharth.Jhawar@dish.com>
2018-07-28 15:04:21 -0600

    Added PATCH endpoint and a test case for the patch endpoint

--- src/main/java/com/example/demo/controller/LessonsController.java
+++ src/main/java/com/example/demo/controller/LessonsController.java
@@ -3,9 +3,15 @@ package com.example.demo.controller;
 
 import com.example.demo.crud.LessonRepository;
 import com.example.demo.model.Lesson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.Optional;
+
+import static org.springframework.http.MediaType.*;
 
 @RestController
 @RequestMapping("/lessons")
@@ -36,6 +42,18 @@ public class LessonsController {
     public String deleteLesson(@PathVariable Long id) {
         this.repository.deleteById(id);
         return "Hello";
+    }
+
+    @PatchMapping(value = "/{id}")
+    public String patchLesson(@PathVariable Long id ,@RequestBody Lesson updatedLesson) throws JsonProcessingException {
+        Lesson lesson = this.repository.findById(id).get();
+        lesson.setDeliveredOn(updatedLesson.getDeliveredOn());
+        lesson.setTitle(updatedLesson.getTitle());
+        this.repository.save(lesson);
+
+        ObjectMapper mapper =  new ObjectMapper();
+        return  mapper.writeValueAsString(lesson);
+
     }
 
 }
--- src/test/java/com/example/demo/controller/PatchLessonTest.java
+++ src/test/java/com/example/demo/controller/PatchLessonTest.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+
 import com.example.demo.crud.LessonRepository;
 import com.example.demo.model.Lesson;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.core.Is;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -19,11 +22,12 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.Iterator;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
-public class LessonsControllerTest {
+public class PatchLessonTest {
 
     @Autowired
     LessonRepository lessonRepository;
@@ -45,32 +49,36 @@ public class LessonsControllerTest {
         lessonRepository.save(lesson1);
     }
 
-    @Test
-    @Transactional
-    public void getLessonTest() throws Exception {
-        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/lessons")
-                .contentType(MediaType.APPLICATION_JSON);
-
-        mvc.perform(request)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].title", Is.is("Learning Spring")));
+    @After
+    public void destroy() {
+        lesson = null;
+        lesson1 = null;
     }
 
     @Test
     @Transactional
-    public void postLessonTest() throws Exception {
-
+    @Rollback
+    public void patchLessonTest() throws Exception {
+        lesson =  new Lesson();
+        lesson.setDeliveredOn(new Date());
+        lesson.setTitle("Learning Spring");
+        lesson1 =  new Lesson();
+        lesson1.setDeliveredOn(new Date());
+        lesson1.setTitle("Learning Java");
+        lessonRepository.save(lesson);
+        lessonRepository.save(lesson1);
         Lesson lesson3 = new Lesson();
-        lesson3.setTitle("Learning CloudFoundry");
-        lesson3.setDeliveredOn(new Date());
-        lessonRepository.save(lesson3);
+        lesson3.setTitle("Learning Kubernetes");
+        Iterable<Lesson> all = lessonRepository.findAll();
+        Iterator i = all.iterator();
+        Lesson l = (Lesson) i.next();
         ObjectMapper mapper =  new ObjectMapper();
-        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/lessons")
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.patch(String.format("/lessons/%d", l.getId()))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(lesson3));
         mvc.perform(request)
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title", Is.is("Learning CloudFoundry")));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", Is.is("Learning Kubernetes")));
     }
 
 
